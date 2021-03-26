5027ae8ebf991722c59083099df2c4fb94263f94
Nicholas Reeder <nickreeder32@gmail.com>
2021-03-26 14:00:43 -0700

    replace card with custom html selctor

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,12 @@
-$(document).ready(function () {
+// Site uses branching to return the result. - we'll use addition sum to return language
+// jQuery is used to show and hide the result after questions are answered and submitted. (Tip: test your app several times with different answers.) - #output
+//
+// Business logic
 
+
+// UI Logic
+$(document).ready(function () {
+  $('#dropdown-animal button').on('click', function () {
+    $('#datebox').val($(this).text());
+  });
 })
